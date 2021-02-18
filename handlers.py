6f4c6615e13dc6559135bac02f04e45bc6ce9556
MerlinRed <knight_j_k@mail.ru>
2021-02-18 19:09:51 +0500

    фикс проблемы с выбором города v0.19

--- handlers.py
+++ handlers.py
@@ -37,22 +37,30 @@ def auth_reg(message):
         bot.send_message(chat_id=message.chat.id, text='Вы не авторизованы. Доступ к боту закрыт.')
     else:
         alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'
+        inline_button_enter_your_city = types.InlineKeyboardButton(text='Ввести свой город самостоятельно',
+                                                                   callback_data='Ввести свой город самостоятельно')
         inline_button_choice_letter = [types.InlineKeyboardButton(text=f'{letter}', callback_data=f'{letter}') for
                                        letter in alphabet]
-        inline_markup_choice_letter = types.InlineKeyboardMarkup().add(*inline_button_choice_letter)
+        inline_markup_choice_letter = types.InlineKeyboardMarkup().add(inline_button_enter_your_city,
+                                                                       *inline_button_choice_letter)
         bot.send_message(message.chat.id, 'Нажмите на начальную букву вашего города.',
                          reply_markup=inline_markup_choice_letter)
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def choice_city(callback):
-    alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'
-    for char in alphabet:
-        if callback.data == char:
-            cities = search_file_with_cites('А')
-            inline_button_choice_city = [types.InlineKeyboardButton(text=f'{city}', callback_data=f'{city}') for city in
-                                         cities]
-            inline_markup_choice_city = types.InlineKeyboardMarkup().add(*inline_button_choice_city)
-            bot.send_message(chat_id=callback.message.chat.id,
-                             text='Выберите город и дату, чтобы посмотреть мероприятия.',
-                             reply_markup=inline_markup_choice_city)
+    if callback.data == 'Ввести свой город самостоятельно':
+        bot.send_message(chat_id=callback.message.chat.id, text='Введите ваш город')
+    else:
+        alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'
+        for char in alphabet:
+            if callback.data == char:
+                letter = str(char).upper()
+                cities = search_file_with_cites(letter)
+                inline_button_choice_city = [types.InlineKeyboardButton(text=f'{city}', callback_data=f'{city}') for
+                                             city in
+                                             cities]
+                inline_markup_choice_city = types.InlineKeyboardMarkup().add(*inline_button_choice_city)
+                bot.send_message(chat_id=callback.message.chat.id,
+                                 text='Выберите ваш город.',
+                                 reply_markup=inline_markup_choice_city)
