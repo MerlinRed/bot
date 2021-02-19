307240cb4db07885c20aa6aa3862822d6458073d
MerlinRed <knight_j_k@mail.ru>
2021-02-19 17:22:40 +0500

    совместная работа выбора города и даты, реализация записи v0.10.2

--- select_city_and_date.py
+++ select_city_and_date.py
@@ -5,7 +5,7 @@ from manipulation_with_cities_file import look_all_cities
 from manipulation_with_cities_file import search_file_with_cites
 
 CITY = None
-DATE = ''
+DATE = None
 
 
 @bot.callback_query_handler(func=lambda callback: True)
@@ -109,5 +109,6 @@ def writing_selected_city(message, city):
 
 def writing_selected_date(message, date):
     global DATE
+    DATE = ''
     DATE += ' ' + date
     bot.send_message(chat_id=message.chat.id, text=f'дата {DATE}')
