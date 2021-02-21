43695ada2891a696652bf006f546ca3135e363b6
MerlinRed <knight_j_k@mail.ru>
2021-02-22 01:01:14 +0500

    пробую убирать глобальные переменные v0.3

--- select_city_and_date.py
+++ select_city_and_date.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from telebot import types
 
 from afisha import take_and_translate_city_for_search, transform_date, select_event
@@ -5,23 +7,30 @@ from load_all import bot
 from manipulation_with_cities_file import look_all_cities
 from manipulation_with_cities_file import search_file_with_cites
 
-YEAR = None
-MONTH = None
-DAY = None
-DATE = None
+
+@dataclass
+class City:
+    city: str
+
+
+@dataclass
+class Date:
+    year: str
+    month: str
+    day: str
+
+    @property
+    def date(self):
+        return f'{Date.year}-{Date.month}-{Date.year}'
 
 
 @bot.callback_query_handler(func=lambda callback: True)
 def choice_city(callback):
-    global YEAR
-    global MONTH
-    global DAY
-    user_city = None
     alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЭЮЯ'
     years = ['2021', '2022', '2023', '2024']
     if callback.data == 'Ввести свой город самостоятельно':
-        bot.send_message(chat_id=callback.message.chat.id, text='Введите ваш город')
-        user_city = take_and_translate_city_for_search(callback.message.text)
+        msg_entered_city = bot.send_message(chat_id=callback.message.chat.id, text='Введите ваш город')
+        bot.register_next_step_handler(msg_entered_city, writing_entered_city)
 
     for char in alphabet:
         if callback.data == char:
@@ -29,37 +38,35 @@ def choice_city(callback):
 
     for city in [city for city in look_all_cities()]:
         if callback.data == city:
-            user_city = take_and_translate_city_for_search(callback.data)
-            bot.send_message(chat_id=callback.message.chat.id, text=f'город - {user_city}')
-            years_in_calendar(message=callback.message)
+            writing_selected_city(message=callback.message, city=callback.data)
 
     for year in years:
         if callback.data == year:
-            YEAR = callback.data
+            Date.year = callback.data
             months_in_calendar(message=callback.message)
 
     if callback.data in ['Январь', 'Март', 'Май', 'Июль', 'Август', 'Октябрь', 'Декабрь']:
-        MONTH = callback.data
+        Date.month = transform_date(month=callback.data)
         days_in_calendar(message=callback.message, quantity_days=31)
     elif callback.data in ['Апрель', 'Июнь', 'Сентябрь', 'Ноябрь']:
-        MONTH = callback.data
+        Date.month = transform_date(month=callback.data)
         days_in_calendar(message=callback.message, quantity_days=30)
     elif callback.data == 'Февраль':
-        MONTH = callback.data
+        Date.month = transform_date(month=callback.data)
         days_in_calendar(message=callback.message, quantity_days=28)
 
     if callback.data in ['0' + str(x) if x in [1, 2, 3, 4, 5, 6, 7, 8, 9] else str(x) for x in range(1, 31 + 1)]:
-        DAY = callback.data
-        writing_selected_date(message=callback.message)
+        Date.day = callback.data
+        difference_events(callback.message)
 
     if callback.data == 'Концерты':
-        select_event(message=callback.message, city=user_city, date=DATE, concert=True)
+        select_event(message=callback.message, city=City.city, date=Date.date, concert=True)
 
     elif callback.data == 'Спектакли':
-        select_event(message=callback.message, city=user_city, date=DATE, performance=True)
+        select_event(message=callback.message, city=City.city, date=Date.date, performance=True)
 
     elif callback.data == 'Выставки':
-        select_event(message=callback.message, city=user_city, date=DATE, exhibition=True)
+        select_event(message=callback.message, city=City.city, date=Date.date, exhibition=True)
 
 
 def select_letter_your_city(message):
@@ -124,9 +131,13 @@ def difference_events(message):
                      reply_markup=inline_markup_choice_event)
 
 
-def writing_selected_date(message):
-    global DATE
-    month = transform_date(month=MONTH)
-    DATE = YEAR + '-' + month + '-' + DAY
-    bot.send_message(chat_id=message.chat.id, text=f'дата - {DATE}')
-    difference_events(message)
+def writing_entered_city(message):
+    City.city = take_and_translate_city_for_search(city=message.text)
+    bot.send_message(chat_id=message.chat.id, text=f'Вы ввели город - {City.city}')
+    years_in_calendar(message=message)
+
+
+def writing_selected_city(message, city):
+    City.city = take_and_translate_city_for_search(city=city)
+    bot.send_message(chat_id=message.chat.id, text=f'Вы выбрали город - {City.city}')
+    years_in_calendar(message=message)
