f26d57a453328f68aa8829e187a3c26244a98029
MerlinRed <knight_j_k@mail.ru>
2021-02-22 15:46:23 +0500

    добавил обработку ошибок

--- afisha.py
+++ afisha.py
@@ -22,79 +22,99 @@ def declension_month(month):
 def select_event(message, city, date, concert=False, exhibition=False, performance=False, movie=False):
     if concert:
         url = f'https://www.afisha.ru/{city}/schedule_concert/{date}/'
+        bot.send_message(chat_id=message.chat.id, text='Идет поиск событий...')
         search_concert(message=message, url=url)
     elif exhibition:
         url = f'https://www.afisha.ru/{city}/schedule_exhibition/{date}/'
+        bot.send_message(chat_id=message.chat.id, text='Идет поиск событий...')
         search_exhibition(message=message, url=url)
     elif performance:
         url = f'https://www.afisha.ru/{city}/schedule_theatre/{date}/'
+        bot.send_message(chat_id=message.chat.id, text='Идет поиск событий...')
         search_performance(message=message, url=url)
     elif movie:
         url = f'https://www.afisha.ru/{city}/schedule_cinema/{date}/'
+        bot.send_message(chat_id=message.chat.id, text='Идет поиск событий...')
         search_cinema(message=message, url=url)
 
 
 def search_concert(message, url):
-    list_concerts = []
-    response = requests.get(url=url)
-    events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
-    for event in events.find_all('section', {'class': '_3cJdx _2nJif like-container'}):
-        for genre in event.find_all('a', {'class': 'WR4gB'}): ...
-        for name in event.find_all('h3', {'class': 'heHLK'}): ...
-        for time in event.find_all('div', {'class': '_1Jo7v'}): ...
-        list_concerts.append((genre.get_text(), name.get_text(), time.get_text()))
+    try:
 
-    sort_concerts = sorted(set(list_concerts))
-    for concert in sort_concerts:
-        bot.send_message(chat_id=message.chat.id,
-                         text=f'жанр - {concert[0]}\nназвание - {concert[1]}\nместо и дата - {concert[2]}')
+        list_concerts = []
+        response = requests.get(url=url)
+        events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
+        for event in events.find_all('section', {'class': '_3cJdx _2nJif like-container'}):
+            for genre in event.find_all('a', {'class': 'WR4gB'}): ...
+            for name in event.find_all('h3', {'class': 'heHLK'}): ...
+            for time in event.find_all('div', {'class': '_1Jo7v'}): ...
+            list_concerts.append((genre.get_text(), name.get_text(), time.get_text()))
+
+        sort_concerts = sorted(set(list_concerts))
+        for concert in sort_concerts:
+            bot.send_message(chat_id=message.chat.id,
+                             text=f'жанр - {concert[0]}\nназвание - {concert[1]}\nместо и дата - {concert[2]}')
+    except AttributeError:
+        bot.send_message(chat_id=message.chat.id, text='Ничего не найдено.')
 
 
 def search_exhibition(message, url):
-    list_exhibitions = []
-    response = requests.get(url=url)
-    events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
-    for event in events.find_all('section', {'class': '_1ER_u _2nJif like-container'}):
-        for genre in event.find_all('a', {'class': 'WR4gB'}): ...
-        for name in event.find_all('h3', {'class': 'heHLK'}): ...
-        for time in event.find_all('div', {'class': '_1Jo7v'}): ...
-        list_exhibitions.append((genre.get_text(), name.get_text(), time.get_text()))
+    try:
+        list_exhibitions = []
+        response = requests.get(url=url)
+        events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
+        for event in events.find_all('section', {'class': '_1ER_u _2nJif like-container'}):
+            for genre in event.find_all('a', {'class': 'WR4gB'}): ...
+            for name in event.find_all('h3', {'class': 'heHLK'}): ...
+            for time in event.find_all('div', {'class': '_1Jo7v'}): ...
+            list_exhibitions.append((genre.get_text(), name.get_text(), time.get_text()))
 
-    sort_exhibitions = sorted(set(list_exhibitions))
-    for exhibition in sort_exhibitions:
-        bot.send_message(chat_id=message.chat.id,
-                         text=f'жанр - {exhibition[0]}\nназвание - {exhibition[1]}\n' \
-                              f'место и дата окончания выстовки - {exhibition[2]}')
+        sort_exhibitions = sorted(set(list_exhibitions))
+        for exhibition in sort_exhibitions:
+            bot.send_message(chat_id=message.chat.id,
+                             text=f'жанр - {exhibition[0]}\nназвание - {exhibition[1]}\n' \
+                                  f'место и дата окончания выстовки - {exhibition[2]}')
+    except AttributeError:
+        bot.send_message(chat_id=message.chat.id, text='Ничего не найдено.')
 
 
 def search_performance(message, url):
-    list_performance = []
-    response = requests.get(url=url)
-    events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
-    for event in events.find_all('section', {'class': 'jP8J- _2nJif like-container'}):
-        for genre in event.find_all('a', {'class': 'WR4gB'}): ...
-        for name in event.find_all('h3', {'class': 'heHLK'}): ...
-        for time in event.find_all('div', {'class': '_1Jo7v'}): ...
-        list_performance.append((genre.get_text(), name.get_text(), time.get_text()))
+    try:
 
-    sort_performance = sorted(set(list_performance))
-    for performance in sort_performance:
-        bot.send_message(chat_id=message.chat.id,
-                         text=f'жанр - {performance[0]}\nназвание - {performance[1]}\n' \
-                              f'место и дата - {performance[2]}')
+        list_performance = []
+        response = requests.get(url=url)
+        events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
+        for event in events.find_all('section', {'class': 'jP8J- _2nJif like-container'}):
+            for genre in event.find_all('a', {'class': 'WR4gB'}): ...
+            for name in event.find_all('h3', {'class': 'heHLK'}): ...
+            for time in event.find_all('div', {'class': '_1Jo7v'}): ...
+            list_performance.append((genre.get_text(), name.get_text(), time.get_text()))
+
+        sort_performance = sorted(set(list_performance))
+        for performance in sort_performance:
+            bot.send_message(chat_id=message.chat.id,
+                             text=f'жанр - {performance[0]}\nназвание - {performance[1]}\n' \
+                                  f'место и дата - {performance[2]}')
+    except AttributeError:
+        bot.send_message(chat_id=message.chat.id, text='Ничего не найдено.')
 
 
 def search_cinema(message, url):
-    list_movies = []
-    response = requests.get(url=url)
-    events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
-    for event in events.find_all('section', {'class': 'oIhSV _2nJif like-container'}):
-        for genre in event.find_all('a', {'class': 'WR4gB'}): ...
-        for name in event.find_all('h3', {'class': 'heHLK'}): ...
-        description = [div for div in event.select('div') if not div.has_attr('class')]
-        list_movies.append((genre.get_text(), name.get_text(), description[0].get_text()))
+    try:
 
-    sort_movies = sorted(set(list_movies))
-    for movie in sort_movies:
-        bot.send_message(chat_id=message.chat.id,
-                         text=f'жанр - {movie[0]}\nназвание - {movie[1]}\nописание - {movie[2]}')
+        list_movies = []
+        response = requests.get(url=url)
+        events = BeautifulSoup(response.content, 'html.parser').find('div', class_='content content_view_cards')
+        for event in events.find_all('section', {'class': 'oIhSV _2nJif like-container'}):
+            for genre in event.find_all('a', {'class': 'WR4gB'}): ...
+            for name in event.find_all('h3', {'class': 'heHLK'}): ...
+            description = [div for div in event.select('div') if not div.has_attr('class')]
+            list_movies.append((genre.get_text(), name.get_text(), description[0].get_text()))
+
+        sort_movies = sorted(set(list_movies))
+        for movie in sort_movies:
+            bot.send_message(chat_id=message.chat.id,
+                             text=f'жанр - {movie[0]}\nназвание - {movie[1]}\nописание - {movie[2]}')
+
+    except AttributeError:
+        bot.send_message(chat_id=message.chat.id, text='Ничего не найдено.')
