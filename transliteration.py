ea591e0772ec8ea7fec5499b287af680d6363a8d
MerlinRed <knight_j_k@mail.ru>
2021-02-22 17:30:48 +0500

    отправка городов на тест

--- transliteration.py
+++ transliteration.py
@@ -22,29 +22,22 @@ def transliteration_data(data):
         'т': 't',
         'у': 'u',
         'ф': 'f',
-        'х': 'h',
+        'х': 'kh',
         'ц': 'c',
         'ч': 'ch',
         'ш': 'sh',
         'щ': 'sh',
-        'ъ': '',
         'ы': 'i',
-        'ь': '',
         'э': 'e',
         'ю': 'u',
-        'я': 'ya'}
+        'я': 'ya',
+        ' ': '_'}
 
     translit_data = data.lower()
     if translit_data == 'москва':
         return 'msk'
     elif translit_data == 'санкт-петербург':
         return 'spb'
-    elif translit_data == 'архангельск':
-        return 'arkhangelsk'
-    elif translit_data == 'тюмень':
-        return 'tumen'
-    elif translit_data == 'астрахань':
-        return 'astrakhan'
     else:
         for key in transliteration_letters:
             translit_data = translit_data.replace(key, transliteration_letters[key])
