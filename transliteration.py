63ea7b2926e80135388d9a7d12e081f13409c5c5
MerlinRed <knight_j_k@mail.ru>
2021-02-22 16:34:48 +0500

    фикс поломки при нажатии на определенный город

--- transliteration.py
+++ transliteration.py
@@ -41,27 +41,9 @@ def transliteration_data(data):
         return 'spb'
     elif translit_data == 'архангельск':
         return 'arkhangelsk'
-    elif translit_data == 'йошкар-ола':
-        return 'yoshkar_ola'
-    elif translit_data == 'ханты-мансийск':
-        return 'hanti_mansijsk'
-    elif translit_data == 'великий новгород':
-        return 'veliky_novgorod'
-    elif translit_data == 'верхняя пышма':
-        return 'verchnaya_pishma'
-    elif translit_data == 'каменск-уральский':
-        return 'kamensk_uralskij'
-    elif translit_data == 'нижний новгород':
-        return 'nnovgorod'
-    elif translit_data == 'нижний тагил':
-        return 'nizhny_tagil'
-    elif translit_data == 'набережные челны':
-        return 'naberezhnie_chelni'
-    elif translit_data == 'улан-удэ':
-        return 'ulan_ude'
-    elif translit_data == 'южно-сахалинск':
-        return 'yuzhno-sakhalinsk'
     else:
         for key in transliteration_letters:
             translit_data = translit_data.replace(key, transliteration_letters[key])
         return translit_data.lower()
+
+print(transliteration_data('Тюмень'))
