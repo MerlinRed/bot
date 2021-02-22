bb02dd5e2e980dd71e58011a7bfb40744b866654
MerlinRed <knight_j_k@mail.ru>
2021-02-22 16:19:31 +0500

    сделал обработку некоторых городов, все не проверял

--- transliteration.py
+++ transliteration.py
@@ -10,7 +10,7 @@ def transliteration_data(data):
         'ж': 'zh',
         'з': 'z',
         'и': 'i',
-        'й': 'i',
+        'й': 'y',
         'к': 'k',
         'л': 'l',
         'м': 'm',
@@ -26,12 +26,12 @@ def transliteration_data(data):
         'ц': 'c',
         'ч': 'ch',
         'ш': 'sh',
-        'щ': 'scz',
+        'щ': 'sh',
         'ъ': '',
-        'ы': 'y',
+        'ы': 'i',
         'ь': '',
         'э': 'e',
-        'ю': 'yu',
+        'ю': 'u',
         'я': 'ya'}
 
     translit_data = data.lower()
@@ -41,7 +41,30 @@ def transliteration_data(data):
         return 'spb'
     elif translit_data == 'архангельск':
         return 'arkhangelsk'
+    elif translit_data == 'йошкар-ола':
+        return 'yoshkar_ola'
+    elif translit_data == 'ханты-мансийск':
+        return 'hanti_mansijsk'
+    elif translit_data == 'великий новгород':
+        return 'veliky_novgorod'
+    elif translit_data == 'верхняя пышма':
+        return 'verchnaya_pishma'
+    elif translit_data == 'каменск-уральский':
+        return 'kamensk_uralskij'
+    elif translit_data == 'нижний новгород':
+        return 'nnovgorod'
+    elif translit_data == 'нижний тагил':
+        return 'nizhny_tagil'
+    elif translit_data == 'набережные челны':
+        return 'naberezhnie_chelni'
+    elif translit_data == 'улан-удэ':
+        return 'ulan_ude'
+    elif translit_data == 'южно-сахалинск':
+        return 'yuzhno-sakhalinsk'
     else:
         for key in transliteration_letters:
             translit_data = translit_data.replace(key, transliteration_letters[key])
         return translit_data.lower()
+
+
+print(transliteration_data('Ханты-Мансийск'))
