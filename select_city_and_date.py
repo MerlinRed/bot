0fe6ec7623ce39d9704abd5233fdfd0aa5e45c6f
MerlinRed <knight_j_k@mail.ru>
2021-03-12 00:56:22 +0500

    заменил call.message.from_user.id на call.from_user.id

--- select_city_and_date.py
+++ select_city_and_date.py
@@ -23,7 +23,7 @@ class Date:
 def choice_city(callback):
     alphabet = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЧШЩЭЮЯ'
     if callback.data == 'Ввести свой город самостоятельно':
-        msg_entered_city = bot.send_message(chat_id=callback.from_user.id, text='Введите ваш город')
+        msg_entered_city = bot.send_message(chat_id=callback.message.chat.id, text='Введите ваш город')
         bot.register_next_step_handler(msg_entered_city, writing_entered_city)
 
     for char in alphabet:
